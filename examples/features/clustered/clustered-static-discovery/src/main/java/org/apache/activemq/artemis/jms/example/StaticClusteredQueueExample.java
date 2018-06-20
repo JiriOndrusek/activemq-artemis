806be5c1b4d297a6029d84b2de8ce19be38784fe
JiriOndrusek <jondruse@redhat.com>
2018-06-20 15:01:44 +0200

    reproducer for https://issues.jboss.org/browse/JBEAP-5252 without eap

--- examples/features/clustered/clustered-static-discovery/src/main/java/org/apache/activemq/artemis/jms/example/StaticClusteredQueueExample.java
+++ examples/features/clustered/clustered-static-discovery/src/main/java/org/apache/activemq/artemis/jms/example/StaticClusteredQueueExample.java
@@ -41,16 +41,16 @@ public class StaticClusteredQueueExample {
 
       Connection connection1 = null;
 
-      Connection connection2 = null;
-
-      Connection connection3 = null;
+//      Connection connection2 = null;
+//
+//      Connection connection3 = null;
 
       try {
          // Step 2. Use direct instantiation (or JNDI if you like)
          Queue queue = ActiveMQJMSClient.createQueue("exampleQueue");
 
          // Step 3. new JMS Connection Factory object from JNDI on server 3
-         ConnectionFactory cf0 = new ActiveMQConnectionFactory("tcp://localhost:61619");
+         ConnectionFactory cf0 = new ActiveMQConnectionFactory("tcp://node2.domain:61617");
 
          //grab an initial connection and wait, in reality you wouldn't do it this way but since we want to ensure an
          // equal load balance we do this and then create 4 connections round robined
@@ -63,11 +63,11 @@ public class StaticClusteredQueueExample {
          // Step 7. We create a JMS Connection connection1 which is a connection to server 1
          connection1 = cf0.createConnection();
 
-         // Step 6. We create a JMS Connection connection0 which is a connection to server 2
-         connection2 = cf0.createConnection();
-
-         // Step 7. We create a JMS Connection connection1 which is a connection to server 3
-         connection3 = cf0.createConnection();
+//         // Step 6. We create a JMS Connection connection0 which is a connection to server 2
+//         connection2 = cf0.createConnection();
+//
+//         // Step 7. We create a JMS Connection connection1 which is a connection to server 3
+//         connection3 = cf0.createConnection();
 
          // Step 8. We create a JMS Session on server 0
          Session session0 = connection0.createSession(false, Session.AUTO_ACKNOWLEDGE);
@@ -75,34 +75,34 @@ public class StaticClusteredQueueExample {
          // Step 9. We create a JMS Session on server 1
          Session session1 = connection1.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-         // Step 8. We create a JMS Session on server 2
-         Session session2 = connection2.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-         // Step 9. We create a JMS Session on server 3
-         Session session3 = connection3.createSession(false, Session.AUTO_ACKNOWLEDGE);
+//         // Step 8. We create a JMS Session on server 2
+//         Session session2 = connection2.createSession(false, Session.AUTO_ACKNOWLEDGE);
+//
+//         // Step 9. We create a JMS Session on server 3
+//         Session session3 = connection3.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
          // Step 10. We start the connections to ensure delivery occurs on them
          connection0.start();
 
          connection1.start();
 
-         connection2.start();
-
-         connection3.start();
+//         connection2.start();
+//
+//         connection3.start();
 
          // Step 11. We create JMS MessageConsumer objects on server 0 server 1 server 2 server 3
          MessageConsumer consumer0 = session0.createConsumer(queue);
 
          MessageConsumer consumer1 = session1.createConsumer(queue);
 
-         MessageConsumer consumer2 = session2.createConsumer(queue);
-
-         MessageConsumer consumer3 = session3.createConsumer(queue);
+//         MessageConsumer consumer2 = session2.createConsumer(queue);
+//
+//         MessageConsumer consumer3 = session3.createConsumer(queue);
 
          Thread.sleep(2000);
 
          // Step 12. We create a JMS MessageProducer object on server 3
-         MessageProducer producer = session3.createProducer(queue);
+         MessageProducer producer = session1.createProducer(queue);
 
          // Step 13. We send some messages to server 3
 
@@ -122,14 +122,17 @@ public class StaticClusteredQueueExample {
          // maximum of one consumer
          int con0Node = ServerUtil.getServer(connection0);
          int con1Node = ServerUtil.getServer(connection1);
-         int con2Node = ServerUtil.getServer(connection2);
-         int con3Node = ServerUtil.getServer(connection3);
-
-         if (con0Node + con1Node + con2Node + con3Node != 6) {
+//         int con2Node = ServerUtil.getServer(connection2);
+//         int con3Node = ServerUtil.getServer(connection3);
+         if (con0Node + con1Node/* + con2Node + con3Node */!= 1) {
             throw new IllegalStateException();
          }
-         for (int i = 0; i < numMessages; i += 4) {
+         for (int i = 0; i < numMessages; i += 2) {
             TextMessage message0 = (TextMessage) consumer0.receive(5000);
+
+            if(message0 == null) {
+               throw new IllegalStateException("Bridge is not working");
+            }
 
             System.out.println("Got message: " + message0.getText() + " from node " + con0Node);
 
@@ -137,13 +140,13 @@ public class StaticClusteredQueueExample {
 
             System.out.println("Got message: " + message1.getText() + " from node " + con1Node);
 
-            TextMessage message2 = (TextMessage) consumer2.receive(5000);
-
-            System.out.println("Got message: " + message2.getText() + " from node " + con2Node);
-
-            TextMessage message3 = (TextMessage) consumer3.receive(5000);
-
-            System.out.println("Got message: " + message3.getText() + " from node " + con3Node);
+//            TextMessage message2 = (TextMessage) consumer2.receive(5000);
+//
+//            System.out.println("Got message: " + message2.getText() + " from node " + con2Node);
+//
+//            TextMessage message3 = (TextMessage) consumer3.receive(5000);
+//
+//            System.out.println("Got message: " + message3.getText() + " from node " + con3Node);
          }
       } finally {
          // Step 15. Be sure to close our resources!
@@ -160,13 +163,13 @@ public class StaticClusteredQueueExample {
             connection1.close();
          }
 
-         if (connection2 != null) {
-            connection2.close();
-         }
-
-         if (connection3 != null) {
-            connection3.close();
-         }
+//         if (connection2 != null) {
+//            connection2.close();
+//         }
+//
+//         if (connection3 != null) {
+//            connection3.close();
+//         }
       }
    }
 }
